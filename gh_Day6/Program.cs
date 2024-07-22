d0e8106941e262a383c12a152f573b65af7746da
jiyeonUning <yeon14735@gmail.com>
2024-07-22 23:44:51 +0900

    간단한 계산기 기능 구현 완료
    class 와 static 을 사용하여, 사칙연산+제곱을 수행할 수 있는 간단한 계산기를 구현할 수 있었다.

--- gh_Day6/Program.cs
+++ gh_Day6/Program.cs
@@ -1,90 +1,90 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿//using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace gh_Day6
-{
-    internal class Program
-    {
-        class Chracter
-        {                              //         ~ 필드 ~
-            public int Level;          //  = 레벨
-            private int hp = 100;      //  = 체력, 기본값 100 설정
-            public float speed;        //  = 이동속도
-            public int AttackDamage = 10;   //  = 공격력, 기본값 10 설정
+//namespace gh_Day6
+//{
+//    internal class Program
+//    {
+//        class Chracter
+//        {                              //         ~ 필드 ~
+//            public int Level;          //  = 레벨
+//            private int hp = 100;      //  = 체력, 기본값 100 설정
+//            public float speed;        //  = 이동속도
+//            public int AttackDamage = 10;   //  = 공격력, 기본값 10 설정
 
 
-            public void Move()           //        ~ 메서드 ~
-            {
+//            public void Move()           //        ~ 메서드 ~
+//            {
 
-                string input = Console.ReadLine();  // = 방향키 설정.
+//                string input = Console.ReadLine();  // = 방향키 설정.
 
-                switch(input)                       // = 움직임 동작
-                {
-                    case "w":
-                        Console.WriteLine("앞으로 전진 합니다.");
-                        break;
-                    case "s":
-                        Console.WriteLine("뒤로 후진합니다.");
-                        break;
-                    case "a":
-                        Console.WriteLine("90도로 좌회전 합니다.");
-                        break;
-                    case "d":
-                        Console.WriteLine("90도로 우회전 합니다.");
-                        break;
-                }
-            }
+//                switch(input)                       // = 움직임 동작
+//                {
+//                    case "w":
+//                        Console.WriteLine("앞으로 전진 합니다.");
+//                        break;
+//                    case "s":
+//                        Console.WriteLine("뒤로 후진합니다.");
+//                        break;
+//                    case "a":
+//                        Console.WriteLine("90도로 좌회전 합니다.");
+//                        break;
+//                    case "d":
+//                        Console.WriteLine("90도로 우회전 합니다.");
+//                        break;
+//                }
+//            }
 
-            public void Attack(Slime slime)  //  = 공격
-            {
-                Console.WriteLine($"적에게 {AttackDamage} 만큼의 데미지를 입혔습니다!");
-                slime.TakeDamage(AttackDamage);
-            }
+//            public void Attack(Slime slime)  //  = 공격
+//            {
+//                Console.WriteLine($"적에게 {AttackDamage} 만큼의 데미지를 입혔습니다!");
+//                slime.TakeDamage(AttackDamage);
+//            }
 
-            public void TakeDamage(int damage) // = 피격
-            {
-                Console.WriteLine($"플레이어가 {damage}의 데미지를 받았습니다.");
-                hp -= damage;
-                Console.WriteLine($"플레이어의 체력은 현재 {hp} 입니다.");
+//            public void TakeDamage(int damage) // = 피격
+//            {
+//                Console.WriteLine($"플레이어가 {damage}의 데미지를 받았습니다.");
+//                hp -= damage;
+//                Console.WriteLine($"플레이어의 체력은 현재 {hp} 입니다.");
                 
-                if (hp <= 0 ) { Console.WriteLine("플레이어가 쓰러졌습니다..."); }
-            }
-        }
+//                if (hp <= 0 ) { Console.WriteLine("플레이어가 쓰러졌습니다..."); }
+//            }
+//        }
 
-        class Slime  // 테스트를 위해 구현한 적대 대상 : 슬라임
-        {
-            public string name = "슬라임";
-            public int hp = 30;
-            public int attackChracter = 5;
+//        class Slime  // 테스트를 위해 구현한 적대 대상 : 슬라임
+//        {
+//            public string name = "슬라임";
+//            public int hp = 30;
+//            public int attackChracter = 5;
 
-            public void Attack(Chracter chracter)
-            {
-                Console.WriteLine($"{name}이(가) 플레이어에게 {attackChracter}의 데미지를 입혔습니다!");
-                chracter.TakeDamage(attackChracter);
-            }
-
-
-            public void TakeDamage(int damage) 
-            {
-                Console.WriteLine($"{name}이(가) {damage}의 데미지를 받았습니다.");
-                hp -= damage;
-                Console.WriteLine($"{name}의 체력은 현재 {hp} 입니다.");
-
-                if (hp <= 0) { Console.WriteLine($"{name}이(가) 쓰러집니다..."); }
-            }
-        }
+//            public void Attack(Chracter chracter)
+//            {
+//                Console.WriteLine($"{name}이(가) 플레이어에게 {attackChracter}의 데미지를 입혔습니다!");
+//                chracter.TakeDamage(attackChracter);
+//            }
 
 
-        static void Main(string[] args) // 구현 테스트
-        {
-            Chracter chracter = new Chracter();
-            Slime slime = new Slime();
+//            public void TakeDamage(int damage) 
+//            {
+//                Console.WriteLine($"{name}이(가) {damage}의 데미지를 받았습니다.");
+//                hp -= damage;
+//                Console.WriteLine($"{name}의 체력은 현재 {hp} 입니다.");
 
-            slime.Attack(chracter);
-            chracter.Attack(slime);
-            slime.Attack(chracter);
-            chracter.Attack(slime);
-            slime.Attack(chracter);
-            chracter.Attack(slime);
-        }
-    }
-}
+//                if (hp <= 0) { Console.WriteLine($"{name}이(가) 쓰러집니다..."); }
+//            }
+//        }
+
+
+//        static void Main(string[] args) // 구현 테스트
+//        {
+//            Chracter chracter = new Chracter();
+//            Slime slime = new Slime();
+
+//            slime.Attack(chracter);
+//            chracter.Attack(slime);
+//            slime.Attack(chracter);
+//            chracter.Attack(slime);
+//            slime.Attack(chracter);
+//            chracter.Attack(slime);
+//        }
+//    }
+//}
--- gh_Day6/gh_2.cs
+++ gh_Day6/gh_2.cs
@@ -1,39 +1,85 @@
-﻿namespace gh_Day6
+﻿using System;
+
+namespace gh_Day6
 {
     internal class gh_2
     {
-        /*
-         과제 1. 계산기 클래스 만들기
-        주어진 조건을 만족하는 클래스를 설계하시오.
-        클래스 내 연산에 관련된 메서드는 static 으로 프로그램 전역에서 사용할 수 있어야 합니다.
- 
-        요구 사항:
-        클래스 이름
-        MyCalculator
+        // 계산기 제작 조건 :
+        // 1. 클래스 내 연산에 관련된 메서드는 모두 static으로, 프로그램 전역에서 사용할 수 있어야한다. = public 사용
+        // 2. 모든 메서드는 두 개의 double 타입 입력을 받고, 결과 또한 double 타입으로 반환해야 한다.
+        // 3. Divide 메서드에서 0으로 나눌 경우, 콘솔에서 에러 발생 텍스트를 출력해야 한다.
 
-        메서드:
-        Add: 두 개의 숫자를 받아 그 합을 반환합니다.
+        static class MyCalculator  // 클래스 이름 : MyCalculator
+        {
+            public static double One;  // static 사용으로 어디서든 사용이 가능해짐
+            public static double Two;
 
-        Subtract: 두 개의 숫자를 받아 첫 번째에서 두 번째를 뺀 결과를 반환합니다.
+            public static double Add()
+            {
+                return One + Two;  // = Add : 두 개의 숫자를 받아 그 합을 반환한다.
+            }
 
-        Multiply: 두 개의 숫자를 받아 그 곱을 반환합니다.
+            public static double Subtract() // = Subtract : 두 개의 숫자를 받아 첫번째 - 두번째 = 결과를 반환한다.
+            {
+                return One - Two;
+            }
 
-        Divide: 두 개의 숫자를 받아 첫 번째를 두 번째로 나눈 결과를 반환합니다. 0으로 나누는 경우에는 적절한 에러 메시지를 반환해야 합니다.
+            public static double Multiply() // = Multiply : 두 개의 숫자를 받아 그 곱을 반환한다.
+            {
+                return One * Two;
+            }
 
-        Squared: 첫 번째 숫자를 기준으로 두 번째 숫자만큼 제곱하는 기능을 추가합니다.
+            public static double Divide() // =  Divide : 두 개의 숫자를 받아 첫 번째 / 두번째 = 결과를 반환한다.
+            {                                                                    
+                return One / Two;
+            }
 
-        입력과 반환 값:
-        모든 메서드는 두 개의 double 타입 입력을 받고, 결과도 double 타입으로 반환해야 합니다.
+            public static double Squared() // = 첫번째 숫자^두번째숫자 의 기능을 추가한다.
+            {
+                return Math.Pow (One, Two); // = 제곱 구현법 : Math.Pow (첫번째 숫자, 두번째 숫자)
+            }
+        }
 
-        에러 처리:
-        Divide 메서드에서 0으로 나눌 경우, 콘솔에 에러 발생텍스트를 출력해야 합니다
-         */
 
         static void Main(string[] args)
         {
+            Console.Write("첫번째 수를 입력하세요. : ");
+            MyCalculator.One = Double.Parse(Console.ReadLine());
+
+            Console.Write("두번째 수를 입력하세요. : ");
+            MyCalculator.Two = Double.Parse(Console.ReadLine());
 
 
+            Console.WriteLine("다음 보기 중 사용하고 싶은 기능을 선택해주세요.");
 
+            Console.WriteLine("1. 덧셈 \n2. 뺄셈 \n3. 곱셈 \n4. 나눗셈 \n5. 제곱");
+
+            string Input = Console.ReadLine();
+
+            switch (Input)
+            {
+                case "1":
+                    Console.WriteLine($"{MyCalculator.One} + {MyCalculator.Two} = {MyCalculator.Add()}");
+                    break;
+
+                case "2":
+                    Console.WriteLine($"{MyCalculator.One} - {MyCalculator.Two} = {MyCalculator.Subtract()}");
+                    break;
+
+                case "3":
+                    Console.WriteLine($"{MyCalculator.One} X {MyCalculator.Two} = {MyCalculator.Multiply()}");
+                    break;
+
+                case "4":
+                    if (MyCalculator.Two <= 0) //  =  0으로 나누는 경우에는 적절한 에러 메시지를 반환해야 한다.     
+                    {  Console.WriteLine($"에러! \n{MyCalculator.Two}는 0보다 작으므로, 해당 계산기에서는 나눌 수 없습니다. \n다른 수를 입력해주세요."); break; }
+                    Console.WriteLine($"{MyCalculator.One} ÷ {MyCalculator.Two} = {MyCalculator.Divide()}");
+                    break;
+
+                case "5":
+                    Console.WriteLine($"{MyCalculator.One} ^ {MyCalculator.Two} = {MyCalculator.Squared()}");
+                    break;
+            }
         }
     }
 }
